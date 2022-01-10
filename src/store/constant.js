175443212816109842ad0d7a307ba9d4d7ab88d9
Arshaluys Arakelyan <arshaluys_arakelyan@epam.com>
2022-01-10 17:27:04 +0400

    changes in constant

--- src/store/constant.js
+++ src/store/constant.js
@@ -133,7 +133,7 @@ export const CLIENT_LIST = [
         client: 'John',
         date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`,
         product: 'mouse',
-        price: 5,
+        price: 15,
         quantity: 150,
         summary: 7 * 150,
         status: 'done',
